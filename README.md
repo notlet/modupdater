be5998839f0a1472bbd29886e1036d39cb6b703f
let <let@notlet.dev>
2024-06-12 19:30:47 +0300

    0.6.0 Binary
    - modupdater now bundles into a binary, so it no longer depends on nodejs and npm
     - automatic building with github actions
     - moved from nodejs to bunjs
     - updated README
     - updated dependencies
     - bugfixes

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # HOW TO USE MOD UPDATER
 1. Download the [latest release](https://github.com/notlet/modupdater/releases).
-2. Unzip all the contents into a subfolder in your instance's `.minecraft` folder. (for example, in `.minecraft/modupdater`)
-3. Make sure you have [NodeJS](https://nodejs.org/en) and NPM (packed with Node) installed.
-4. To use the updater, run the `run.bat` file (Windows) or `run.sh` (Linux). You can add a shortcut to it on your desktop aswell. If it gives a `MODULE_NOT_FOUND` error, try removing the `node_modules` directory and running again.
+2. Unzip the binary into your instance's `.minecraft` folder.
+3. Run the binary. If you are on Linux, make sure to make it executable with `chmod +x ./modupdater`
 
 To navigate in updater's menu, use arrow keys and enter. To select items in a multiple choice menu, navigate to it with arrows and press Space.
 If you find any bugs, please report them to me, and I'll push out a fix asap.
--- modupdater.js
+++ modupdater.js
@@ -8,7 +8,6 @@ const http = require('http');
 const https = require('https');
 const crypto = require('crypto');
 const progress = require('progress');
-const figlet = require("figlet");
 
 const { version } = require('./package.json');
 const serverurl = "https://mods.notlet.dev";
@@ -43,10 +42,17 @@ const download = (url, filename, silent) => new Promise(async (res, rej) => {
     writer.on('error', rej);
 });
 
+const exitOnKey = () => {
+    console.log('\nPress any key to exit.');
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+    process.stdin.on('data', process.exit.bind(process, 1));
+}
+
 const error = e => {
     if (process.argv.includes('--debug')) console.error(e);
     else console.log(`\n${chalk.bold('An error occured or a prompt was cancelled.')}${e ? `\n${chalk.red(e.message)}` : ""}`);
-    process.exit(1);
+    exitOnKey();
 }
 
 (async () => {
@@ -55,21 +61,22 @@ const error = e => {
 
     // verify that we are in proper directory by checking some files
     const checks = [
-        fssync.existsSync('../saves'),
-        fssync.existsSync('../screenshots'),
-        fssync.existsSync('../resourcepacks')
+        fssync.existsSync('./saves'),
+        fssync.existsSync('./screenshots'),
+        fssync.existsSync('./resourcepacks')
     ];
     // fail the program if any of the checks fail
-    if (checks.includes(false) && !process.argv.includes('--debug') && !fssync.existsSync('nodircheck.txt')) throw new Error(`Directory checks failed! You might have to launch minecraft at least once before running this, or you have unzipped this into the wrong directory.\nIf you believe this is an error, you can add a blank file called ${chalk.bold("nodircheck.txt")} in this directory to skip the checks.`);
+    if (checks.includes(false) && !process.argv.includes('--debug') && !fssync.existsSync('nodircheck.txt')) throw new Error(`Directory checks failed! You might have to launch Minecraft at least once, or you are in the wrong directory.\nIf you believe this is an error, you can add a blank file called ${chalk.bold("nodircheck.txt")} in this directory to skip the checks.`);
 
     // check for needed files and folders and create if doesnt exist
-    if (!fssync.existsSync('../mods')) await fs.mkdir('../mods');
+    if (!fssync.existsSync('./mods')) await fs.mkdir('./mods');
     if (!fssync.existsSync('blacklist.json')) await fs.writeFile('blacklist.json', '[]');
 
     if (fssync.existsSync('temp')) await fs.rm('temp', { recursive: true });
     await fs.mkdir('temp');
 
-    console.log(figlet.textSync('sussy smp', { font: 'Larry 3D' }))
+    // packaging with bun breaks figlet, so we gotta do this messy method instead
+    console.log('  ____  __  __    ____    ____  __  __        ____    ___ ___   _____   \n' +  " /',__\\/\\ \\/\\ \\  /',__\\  /',__\\/\\ \\/\\ \\      /',__\\ /' __` __`\\/\\ '__`\\ \n" +  '/\\__, `\\ \\ \\_\\ \\/\\__, `\\/\\__, `\\ \\ \\_\\ \\    /\\__, `\\/\\ \\/\\ \\/\\ \\ \\ \\L\\ \\\n' +  '\\/\\____/\\ \\____/\\/\\____/\\/\\____/\\/`____ \\   \\/\\____/\\ \\_\\ \\_\\ \\_\\ \\ ,__/\n' +  ' \\/___/  \\/___/  \\/___/  \\/___/  `/___/> \\   \\/___/  \\/_/\\/_/\\/_/\\ \\ \\/ \n' +  '                                    /\\___/                        \\ \\_\\ \n' +  '                                    \\/__/                          \\/_/ ')
 
     // check github api for any new versions
     const githubResponse = await axios.get('https://api.github.com/repos/notlet/modupdater/releases/latest').catch(() => console.log(chalk.gray(`Failed to check for new updates.`)));
@@ -80,7 +87,7 @@ const error = e => {
 
     // get mod lists
     const modlistspinner = ora('Fetching mod lists...').start();
-    const moddir = (await fs.readdir('../mods')).filter(f => f.endsWith('.jar'));
+    const moddir = (await fs.readdir('./mods')).filter(f => f.endsWith('.jar'));
     const modlisttimeout = setTimeout(() => modlistspinner.text = 'Fetching mod lists... (this is taking a bit long, is the server down?)', 10000);
     const modlistreq = await axios.get(`${serverurl}/mods.json`).catch(e => {
         modlistspinner.fail('Failed to fetch mod lists.');
@@ -104,128 +111,119 @@ const error = e => {
         choices: ['update all', 'update mods', 'update kubejs', 'manage blacklist', 'check mods']
     }).run();
 
-    const blacklist = require('./blacklist.json');
+    const blacklist = JSON.parse(await fs.readFile('./blacklist.json'));
     // check for missing and unneeded mods
     const missingmods = modlist.map(m => !moddir.includes(m.name) ? m.name : null).filter(m => m != null);
     const realunneededmods = moddir.map(m => !modlist.map(mod => mod.name).includes(m) ? m : null).filter(m => m != null);
     const unneededmods = realunneededmods.filter(m => !blacklist.includes(m));
 
-    const listMods = () => {
-        console.log(`\n${chalk.green.bold('+')} Missing mods: ${missingmods.length > 0 ? `${missingmods.map(m => chalk.italic(m)).join(', ')}` : chalk.greenBright('<none>')}`);
-        console.log(`\n${chalk.red.bold('-')} Unneeded mods: ${realunneededmods.length > 0 ? `${realunneededmods.map(m => blacklist.includes(m) ? chalk.gray.bold(m) : chalk.bold(m)).join(', ')}` : chalk.greenBright('<none>')}`);
-    }
+    const actions = {
+        'update all': () => new Promise(async (resolve) => {
+            await actions['update mods']();
+            await actions['update kubejs']();
+            resolve();
+        }),
+        'update mods': () => new Promise(async (resolve) => {
+            try {
+                console.log(chalk.bold('\nUpdating mods...'));
 
-    const updateMods = () => new Promise(async (resolve) => {
-        try {
-            console.log(chalk.bold('\nUpdating mods...'));
+                console.log(`\n${chalk.green.bold('+')} Need to download ${chalk.bold(missingmods.length + ' mods')}.\n${chalk.red.bold('-')} Need to delete ${chalk.bold(unneededmods.length + ' mods')}.${blacklist.length > 0 ? chalk.gray(' (skipping ' + blacklist.length + ' due to blacklist)') : ''}\n`);
 
-            console.log(`\n${chalk.green.bold('+')} Need to download ${chalk.bold(missingmods.length + ' mods')}.\n${chalk.red.bold('-')} Need to delete ${chalk.bold(unneededmods.length + ' mods')}.${blacklist.length > 0 ? chalk.gray(' (skipping ' + blacklist.length + ' due to blacklist)') : ''}\n`);
-
-            if (unneededmods.length > 0) {
-                const delmodsspinner = ora(`\nDeleting ${unneededmods.length} mods...`).start();
-                for (const mod of unneededmods) {
-                    delmodsspinner.text = `Deleting ${chalk.italic(mod)}`;
-                    await fs.unlink('../mods/' + mod);
+                if (unneededmods.length > 0) {
+                    const delmodsspinner = ora(`\nDeleting ${unneededmods.length} mods...`).start();
+                    for (const mod of unneededmods) {
+                        delmodsspinner.text = `Deleting ${chalk.italic(mod)}`;
+                        await fs.unlink('./mods/' + mod);
+                    }
+                    delmodsspinner.succeed('Successfully deleted unneeded mods.');
+                    console.log('');
                 }
-                delmodsspinner.succeed('Successfully deleted unneeded mods.');
-                console.log('');
-            }
 
-            // download missing mods if needed
-            if (missingmods.length > 0) {
-                // download the mod files
-                for (const mod of missingmods) await download(`${serverurl}/files/${mod}`, mod);
-                console.log(`${chalk.green(s.success)} Downloaded mod files.\n`);
+                // download missing mods if needed
+                if (missingmods.length > 0) {
+                    // download the mod files
+                    for (const mod of missingmods) await download(`${serverurl}/files/${mod}`, mod);
+                    console.log(`${chalk.green(s.success)} Downloaded mod files.\n`);
 
-                const modspinner = ora('Starting mods verification...').start();
-                // verify SHA256 hash and move to mods folder
-                for (const mod of missingmods) {
-                    modspinner.text = `Verifying ${chalk.bold(mod)} SHA256 checksum...`;
-                    const modbuffer = await fs.readFile(`temp/${mod}`);
+                    const modspinner = ora('Starting mods verification...').start();
+                    // verify SHA256 hash and move to mods folder
+                    for (const mod of missingmods) {
+                        modspinner.text = `Verifying ${chalk.bold(mod)} SHA256 checksum...`;
+                        const modbuffer = await fs.readFile(`temp/${mod}`);
 
-                    const hashSum = crypto.createHash('sha256').update(modbuffer).digest('hex');
-                    if (modlist.find(m => m.name === mod)?.checksum !== hashSum) {
-                        modspinner.fail(`Failed to verify ${chalk.bold(mod)}.`)
-                        throw new Error(`Failed to verify ${chalk.bold(mod)}! (hash mismatch)\nIs something/someone intercepting your downloads?`);
+                        const hashSum = crypto.createHash('sha256').update(modbuffer).digest('hex');
+                        if (modlist.find(m => m.name === mod)?.checksum !== hashSum) {
+                            modspinner.fail(`Failed to verify ${chalk.bold(mod)}.`)
+                            throw new Error(`Failed to verify ${chalk.bold(mod)}! (hash mismatch)\nThe file might be corrupted or someone may be intercepting your downloads.`);
+                        }
+
+                        modspinner.text = `Successfully verified, moving to ${chalk.bold('mods')} folder...`;
+                        await fs.rename(`temp/${mod}`, `./mods/${mod}`);
+                        modspinner.text = `Done with ${chalk.bold(mod)}.`;
                     }
 
-                    modspinner.text = `Successfully verified, moving to ${chalk.bold('mods')} folder...`;
-                    await fs.rename(`temp/${mod}`, `../mods/${mod}`);
-                    modspinner.text = `Done with ${chalk.bold(mod)}.`;
-                }
+                    modspinner.succeed('All mods verified and moved to mods folder.');
+                } else console.log(`${s.success} No missing mods, skipping download.`);
 
-                modspinner.succeed('All mods verified and moved to mods folder.');
-            } else console.log(`${s.success} No missing mods, skipping download.`);
+                // finish updater
+                console.log(`\n${s.success} All mods are up to date.`);
+                resolve();
+            } catch (e) { error(e); }
+        }),
+        'update kubejs': () => new Promise(async (resolve) => {
+            console.log(chalk.bold('\nUpdating KubeJS scripts...\n'));
 
-            // finish updater
-            console.log(`\n${s.success} All mods are up to date.`);
+            if (fssync.existsSync('temp/kubejs.zip')) await fs.unlink('temp/kubejs.zip');
+
+            const kjsspinner = ora('Preparing...').start();
+
+            kjsspinner.text = `Deleting old scripts...`;
+            await fs.rm('./kubejs', {recursive: true, force: true});
+
+            kjsspinner.text = 'Downloading new scripts...';
+            await download(`${serverurl}/files/kubejs.zip`, 'kubejs.zip', true);
+            kjsspinner.text = 'Downloading scripts complete.';
+
+            kjsspinner.text = 'Reading archive...';
+            const zip = new AdmZip('temp/kubejs.zip');
+
+            kjsspinner.text = 'Extracting archive...';
+            zip.extractAllTo('./kubejs/', true);
+
+            kjsspinner.succeed('All KubeJS scripts are up to date.');
             resolve();
-        } catch (e) { error(e); }
-    });
+        }),
+        'manage blacklist': () => new Promise(async (resolve) => {
+            if (unneededmods.length < 1 && blacklist.length < 1) error(new Error('There were no unneeded mods found!'));
 
-    const updateKJSScripts = () => new Promise(async (resolve) => {
-        console.log(chalk.bold('\nUpdating KubeJS scripts...\n'));
+            const allunneededmods = unneededmods.concat(blacklist);
 
-        if (fssync.existsSync('temp/kubejs.zip')) await fs.unlink('temp/kubejs.zip');
+            console.log('');
+            const newBlacklist = await new enquirer.MultiSelect({
+                name: 'blacklist',
+                message: 'Pick mods to blacklist from automatic deletion, then press ENTER.',
+                limit: unneededmods.length,
+                initial: blacklist,
+                choices: allunneededmods.map(m => ({name: m, value: m}))
+            }).run();
 
-        const kjsspinner = ora('Preparing...').start();
+            console.log(`${chalk.blueBright(s.info)} Updating blacklist from ${chalk.bold(blacklist.length + ' items')} to ${chalk.bold(newBlacklist.length + ' items')}.\n`);
+            if (!(await new enquirer.Confirm({ name: 'deletemods', message: 'Proceed?' }).run())) error(new Error('Cancelled updating blacklist!'));
 
-        kjsspinner.text = `Deleting old scripts...`;
-        await fs.rm('../kubejs', {recursive: true, force: true});
-
-        kjsspinner.text = 'Downloading new scripts...';
-        await download(`${serverurl}/files/kubejs.zip`, 'kubejs.zip', true);
-        kjsspinner.text = 'Downloading scripts complete.';
-
-        kjsspinner.text = 'Reading archive...';
-        const zip = new AdmZip('temp/kubejs.zip');
-
-        kjsspinner.text = 'Extracting archive...';
-        zip.extractAllTo('../kubejs/', true);
-
-        kjsspinner.succeed('All KubeJS scripts are up to date.');
-        resolve();
-    });
-
-    const manageBlacklist = () => new Promise(async (resolve) => {
-        if (unneededmods.length < 1 && blacklist.length < 1) error(new Error('There were no unneeded mods found!'));
-
-        const allunneededmods = unneededmods.concat(blacklist);
-
-        console.log('');
-        const newBlacklist = await new enquirer.MultiSelect({
-            name: 'blacklist',
-            message: 'Pick mods to blacklist from automatic deletion, then press ENTER.',
-            limit: unneededmods.length,
-            initial: blacklist,
-            choices: allunneededmods.map(m => ({name: m, value: m}))
-        }).run();
-
-        console.log(`${chalk.blueBright(s.info)} Updating blacklist from ${chalk.bold(blacklist.length + ' items')} to ${chalk.bold(newBlacklist.length + ' items')}.\n`);
-        if (!(await new enquirer.Confirm({ name: 'deletemods', message: 'Proceed?' }).run())) error(new Error('Cancelled updating blacklist!'));
-
-        await fs.writeFile('blacklist.json', JSON.stringify(newBlacklist));
-        console.log(`${chalk.green(s.success)} Successfully updated blacklist.`);
-    })
+            await fs.writeFile('blacklist.json', JSON.stringify(newBlacklist));
+            console.log(`${chalk.green(s.success)} Successfully updated blacklist.`);
+            resolve();
+        }),
+        'check mods': () => new Promise((resolve) => {
+            console.log(`\n${chalk.green.bold('+')} Missing mods: ${missingmods.length > 0 ? `${missingmods.map(m => chalk.italic(m)).join(', ')}` : chalk.greenBright('<none>')}`);
+            console.log(`\n${chalk.red.bold('-')} Unneeded mods: ${realunneededmods.length > 0 ? `${realunneededmods.map(m => blacklist.includes(m) ? chalk.gray.bold(m) : chalk.bold(m)).join(', ')}` : chalk.greenBright('<none>')}`);
+            resolve()
+        })
+    }
 
     // handle action
-    switch (action) {
-        case 'update all': 
-            await updateMods();
-            await updateKJSScripts();
-            break;
-        case 'update mods':
-            await updateMods();
-            break;
-        case 'update kubejs': 
-            await updateKJSScripts();
-            break;
-        case 'manage blacklist':
-            await manageBlacklist();
-            break;
-        case 'check mods': 
-            listMods();
-            break;
-        default: throw new Error('unknown choice, how did you even get here?')
-    }
+    if (!Object.keys(actions).includes(action)) throw new Error('unknown choice, how did you even get here?');
+
+    await actions[action]();
+    exitOnKey();
 })().catch(error);
